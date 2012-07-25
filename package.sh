6a22928c98ddfa6df5e9f5a54a371e6d7abef68e
IceStar <yubingxing123@gmail.com>
2012-07-25 16:08:07 +0800

    Modify package.sh
    Modify package.sh, rename README.markdown to README.md

--- package.sh
+++ package.sh
@@ -8,9 +8,9 @@ DIST="target/dist"
 # Package structure
 rm -rf $DIST
 mkdir -p $DIST/bin $DIST/lib $DIST/conf
-cp lib_managed/scala_2.7.7/compile/* $DIST/lib
-cp target/scala_2.7.7/notifier_2.7.7-1.0.jar $DIST/lib
-cp target/scala_2.7.7/resources/* $DIST/conf
+cp lib_managed/scala_2.9.2/compile/* $DIST/lib
+cp target/scala_2.9.2/notifier_2.9.2-1.0.jar $DIST/lib
+cp target/scala_2.9.2/resources/* $DIST/conf
 
 cp src/main/bash/* $DIST/bin
 chmod +x $DIST/bin/*
